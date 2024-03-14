b1cdfc00c0189673b6b7115ec1ec6b662e27858e
Eniola011 <yettyagbalu@gmail.com>
2024-03-14 03:22:51 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+!#usr/bin/python3
+"""
+LockBoxes
+"""
+
+
+def canUnlockAll(boxes)
+    """
+    A method that determines if all the boxes can be opened.
+    """
+    opened_boxes = set()
+    opened_boxes.add(0)
+    stack = [0]
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key not in opened_boxes:
+                opened_boxes.add(key)
+                stack.append(key)
+    return len(opened_boxes) == len(boxes)
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Mock Technical Interview
 | Files | Description |
 | ---- | ----------- |
 | Topic:0 | 0x00. Pascal's Triangle |
+| Topic:1 | 0x01-lockboxes |
