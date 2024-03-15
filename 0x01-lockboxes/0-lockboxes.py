5a49231af58605d9c661bcf9029b1e0dfac038ce
Eniola011 <yettyagbalu@gmail.com>
2024-03-15 04:02:46 +0100

    unlock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,13 +8,14 @@ def canUnlockAll(boxes):
     """
     A method that determines if all the boxes can be opened.
     """
-    opened_boxes = set()
-    opened_boxes.add(0)
-    stack = [0]
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key not in opened_boxes:
-                opened_boxes.add(key)
-                stack.append(key)
-    return len(opened_boxes) == len(boxes)
+    opened_boxes = [0]
+    for idx in opened_boxes:
+        for box in boxes[idx]:
+            if box < len(boxes):
+                if box not in opened_boxes:
+                    opened_boxes.append(box)
+    # check the length of the opened boxes and all the boxes.
+    if len(boxes) == len(opened_boxes):
+        return True
+    else:
+        return False
