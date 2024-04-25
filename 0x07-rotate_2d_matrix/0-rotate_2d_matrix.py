7628a17f919fc105e45e60cf48105c21e4c04895
Eniola011 <yettyagbalu@gmail.com>
2024-04-25 04:23:48 +0000

    2D-Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+
+Rotate 2D-Matrix.
+
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+       >> Rotate 2D-Matrix 90 Degrees Clockwise.
+    """
+    number = len(matrix)
+    # exchange the matrix. switch the rows and columns
+    for i in range(number):
+        for j in range(i, number):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # reverse each row
+    for row in matrix:
+        row.reverse()
